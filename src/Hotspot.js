d680df94ef8053947ba8028b9ca0eb9db3969a48
mtbtalha <talha.babar@arbisoft.com.pk>
2021-01-12 17:12:43 +0500

    Update hotspot Style background color

--- src/Hotspot.js
+++ src/Hotspot.js
@@ -11,7 +11,7 @@ class Hotspot extends React.Component {
       top: y + '%',
       left: x + '%',
       fontFamily: 'Sans-Serif',
-      background: 'white',
+      background: 'blue',
       boxShadow: '0px 0px 2px 0px rgba(0,0,0,0.5)',
       pointerEvents: 'auto'
     }
